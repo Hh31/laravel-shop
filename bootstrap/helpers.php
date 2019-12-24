e6cf60a53e028ab70c8a04242b967116a1cce1ba
hefusheng <1315695870@qq.com>
2019-12-24 15:55:30 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,13 @@ function test_helper()
 {
     return 'OK';
 }
+
+/**
+ * 将当前请求的路由名称转换为 CSS 类名称
+ * @return mixed
+ * @author: hefusheng 2019/12/24
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
