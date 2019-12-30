40bc09cd298a3d46c72a1f2dd0029446138c0103
hefusheng <1315695870@qq.com>
2019-12-30 11:45:20 +0800

    用户界面，商品筛选，排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,9 +8,39 @@ use Illuminate\Http\Request;
 class ProductsController extends Controller
 {
     //
-    public function index() {
-        $products = Product::query()->where('on_sale',true)->paginate(16);
-
-        return view('products.index',['products' => $products]);
+    public function index(Request $request) {
+//        $products = Product::query()->where('on_sale',true)->paginate(16);
+//
+//        return view('products.index',['products' => $products]);
+        //创建一个查询构造器
+        $products = Product::query()->where('on_sale',true);
+        //判断是否有提交$search参数，如果有就赋值给$search参数
+        //
+        if ($search = $request->input('search','')) {
+            $like = '%'.$search.'%';
+            $products->where(function ($query) use ($like){
+                $query->where('title','like',$like)
+                    ->OrWhere('description','like',$like)
+                    ->OrWhereHas('skus',function ($query) use ($like){
+                        $query->where('title','like',$like)
+                            ->Orwhere('description','like',$like);
+                    });
+            });
+        }
+        if ($order = $request->input('order','')){
+            if (preg_match('/^(.+)_(asc|desc)$/',$order,$m)){
+                if(in_array($m[1],['price','sold_count','rating'])){
+                    $products->orderBy($m[1],$m[2]);
+                }
+            }
+        }
+        $products = $products->paginate(16);
+        return view('products.index',[
+            'products' => $products,
+            'filters'  => [
+                'search' => $search,
+                'order'  => $order,
+            ],
+        ]);
     }
 }
