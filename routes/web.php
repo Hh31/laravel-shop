f221f3983880ff22418695ebcd79dedee3b80889
hefusheng <1315695870@qq.com>
2020-01-04 22:40:32 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,8 @@ Route::group(['middleware' => ['auth','verified']], function () {
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::get('cart', 'CartController@index')->name('cart.index');//购物车详情
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
 
 Route::get('products/{product}','ProductsController@show')->name('products.show');//商品详情
